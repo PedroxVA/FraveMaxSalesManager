9ad63d211f2e6d15b3ddfa42a8c08fbb2cfb0e16
Julio <jotaenesoluciones@gmail.com>
2023-10-03 15:09:34 -0300

    2 de Julio
    Sin miedo al exito

--- src/fravemaxsalesmanager/entidades/DetalleVenta.java
+++ src/fravemaxsalesmanager/entidades/DetalleVenta.java
@@ -3,5 +3,128 @@ package fravemaxsalesmanager.entidades;
 
 /* @author PedroxVA */
 public class DetalleVenta {
+    
+    private int idDetalleVenta;
+    private int cantidad;
+    private int idVenta;
+    private double precioVenta;
+    private int idProducto;
+    
+    //constructores
+
+    private DetalleVenta() {}
+
+    public DetalleVenta(int idDetalleVenta, int cantidad, int idVenta, double precioVenta, int idProducto) {
+        this.idDetalleVenta = idDetalleVenta;
+        this.cantidad = cantidad;
+        this.idVenta = idVenta;
+        this.precioVenta = precioVenta;
+        this.idProducto = idProducto;
+    }
+
+    public DetalleVenta(int cantidad, int idVenta, double precioVenta, int idProducto) {
+        this.cantidad = cantidad;
+        this.idVenta = idVenta;
+        this.precioVenta = precioVenta;
+        this.idProducto = idProducto;
+    }
+    
+    // Getters & Setters
+
+    public int getIdDetalleVenta() {
+        return idDetalleVenta;
+    }
+
+    public void setIdDetalleVenta(int idDetalleVenta) {
+        this.idDetalleVenta = idDetalleVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public void setPrecioVenta(double precioVenta) {
+        this.precioVenta = precioVenta;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+    
+    // Metodo to String
+
+    @Override
+    public String toString() {
+        return "DetalleVenta{" + "idDetalleVenta=" + idDetalleVenta + ", cantidad=" + cantidad + ", idVenta=" + idVenta + ", precioVenta=" + precioVenta + ", idProducto=" + idProducto + '}';
+    }
+    
+    // Hashcode & Equals
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + this.idDetalleVenta;
+        hash = 67 * hash + this.cantidad;
+        hash = 67 * hash + this.idVenta;
+        hash = 67 * hash + (int) (Double.doubleToLongBits(this.precioVenta) ^ (Double.doubleToLongBits(this.precioVenta) >>> 32));
+        hash = 67 * hash + this.idProducto;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final DetalleVenta other = (DetalleVenta) obj;
+        if (this.idDetalleVenta != other.idDetalleVenta) {
+            return false;
+        }
+        if (this.cantidad != other.cantidad) {
+            return false;
+        }
+        if (this.idVenta != other.idVenta) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.precioVenta) != Double.doubleToLongBits(other.precioVenta)) {
+            return false;
+        }
+        if (this.idProducto != other.idProducto) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+    
+    
+    
 
 }
--- src/fravemaxsalesmanager/entidades/Venta.java
+++ src/fravemaxsalesmanager/entidades/Venta.java
@@ -1,7 +1,102 @@
 
 package fravemaxsalesmanager.entidades;
 
+import java.time.LocalDate;
+import java.util.Objects;
+
 /* @author PedroxVA */
 public class Venta {
+    
+    private int idVenta;
+    private int idCliente;
+    private LocalDate fechaVenta;
+    
+// Constructores
+
+    private Venta() {}
+
+    public Venta(int idVenta, int idCliente, LocalDate fechaVenta) {
+        this.idVenta = idVenta;
+        this.idCliente = idCliente;
+        this.fechaVenta = fechaVenta;
+    }
+
+    public Venta(int idCliente, LocalDate fechaVenta) {
+        this.idCliente = idCliente;
+        this.fechaVenta = fechaVenta;
+    }
+    
+    // Getters and Setters
+
+    public int getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(int idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDate getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(LocalDate fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+    
+    // Metodo to String
+
+    @Override
+    public String toString() {
+        return "Venta{" + "idVenta=" + idVenta + ", idCliente=" + idCliente + ", fechaVenta=" + fechaVenta + '}';
+    }
+    
+    // Hashcode & Equals
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 71 * hash + this.idVenta;
+        hash = 71 * hash + this.idCliente;
+        hash = 71 * hash + Objects.hashCode(this.fechaVenta);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Venta other = (Venta) obj;
+        if (this.idVenta != other.idVenta) {
+            return false;
+        }
+        if (this.idCliente != other.idCliente) {
+            return false;
+        }
+        if (!Objects.equals(this.fechaVenta, other.fechaVenta)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+
+    
+    
 
 }
