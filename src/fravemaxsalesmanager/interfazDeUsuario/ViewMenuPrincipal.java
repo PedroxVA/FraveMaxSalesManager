e04ab8081abc504872fcf7273314cfa34ac1ebba
Julio <jotaenesoluciones@gmail.com>
2023-10-18 16:45:51 -0300

    Nuevo Menu
    Con imagen de fondo 18 de octubre

--- src/fravemaxsalesmanager/interfazDeUsuario/ViewMenuPrincipal.java
+++ src/fravemaxsalesmanager/interfazDeUsuario/ViewMenuPrincipal.java
@@ -38,7 +38,7 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDesktopPane1 = new javax.swing.JDesktopPane();
+        jPanel1 = new fravemaxsalesmanager.interfazDeUsuario.BackgroundPanel();
         jMenuBar2 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -54,15 +54,15 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
 
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 902, Short.MAX_VALUE)
         );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 664, Short.MAX_VALUE)
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 667, Short.MAX_VALUE)
         );
 
         jMenu1.setText("Inventario");
@@ -142,13 +142,11 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(3, 3, 3)
-                .addComponent(jDesktopPane1))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -165,38 +163,38 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenu1ActionPerformed
 
     private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
-      jDesktopPane1.removeAll();
-      jDesktopPane1.repaint();
+      jPanel1.removeAll();
+      jPanel1.repaint();
       ViewAltaInventario inventario = new ViewAltaInventario();
       inventario.setVisible(true);
-      jDesktopPane1.add(inventario);
+      jPanel1.add(inventario);
       inventario.moveToFront();  
     }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void jMbajaymodificacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMbajaymodificacionActionPerformed
-      jDesktopPane1.removeAll();
-      jDesktopPane1.repaint();
+      jPanel1.removeAll();
+      jPanel1.repaint();
       ViewModificaInventario modificacion = new ViewModificaInventario();
       modificacion.setVisible(true);
-      jDesktopPane1.add(modificacion);
+      jPanel1.add(modificacion);
       modificacion.moveToFront();
     }//GEN-LAST:event_jMbajaymodificacionActionPerformed
 
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-      jDesktopPane1.removeAll();
-      jDesktopPane1.repaint();
+      jPanel1.removeAll();
+      jPanel1.repaint();
       ViewClienteNuevo cliente = new ViewClienteNuevo();
       cliente.setVisible(true);
-      jDesktopPane1.add(cliente);
+      jPanel1.add(cliente);
       cliente.moveToFront(); 
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMGestionDeVentasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMGestionDeVentasActionPerformed
-      jDesktopPane1.removeAll();
-      jDesktopPane1.repaint();
+      jPanel1.removeAll();
+      jPanel1.repaint();
       ViewGestionDeVentas ventas = new ViewGestionDeVentas();
       ventas.setVisible(true);
-      jDesktopPane1.add(ventas);
+      jPanel1.add(ventas);
       ventas.moveToFront();
       
     }//GEN-LAST:event_jMGestionDeVentasActionPerformed
@@ -208,11 +206,11 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
     }//GEN-LAST:event_jMenu2MouseClicked
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
-      jDesktopPane1.removeAll();
-      jDesktopPane1.repaint();
+      jPanel1.removeAll();
+      jPanel1.repaint();
       ViewListados listado = new ViewListados();
       listado.setVisible(true);
-      jDesktopPane1.add(listado);
+      jPanel1.add(listado);
       listado.moveToFront();
     }//GEN-LAST:event_jMenuItem3ActionPerformed
 
@@ -254,7 +252,6 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JDesktopPane jDesktopPane1;
     private javax.swing.JMenuItem jMGestionDeVentas;
     private javax.swing.JMenuItem jMbajaymodificacion;
     private javax.swing.JMenu jMenu1;
@@ -266,5 +263,6 @@ public class ViewMenuPrincipal extends javax.swing.JFrame {
     private javax.swing.JMenuItem jMenuItem1;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
